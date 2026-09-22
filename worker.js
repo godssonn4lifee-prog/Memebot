9d84b92cd6601911875130d27306d14215de9b22
godssonn4lifee-prog <godssonn4lifee@gmail.com>
2026-09-22 02:38:14 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,9 @@
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Memebot is running!");
+  },
 
+  async scheduled(event, env, ctx) {
+    console.log("Memebot scheduled task running");
+  }
+};
